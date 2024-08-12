097a0931f595a5724d637bfe3532a000a479bfd7
Prabhav Giriya <prabhavgiriya925@gmail.com>
2024-08-13 00:58:57 +0530

    second

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 // Define the MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/myShop' //myShop is our database name
-
+// const mongoURL = 'mongodb://localhost:27017/myShop' //myShop is our database name
+// const mongoURL = "mongodb+srv://prabhavgiriya925:myhotel@101010@cluster0.0mlnf.mongodb.net/"
+const mongoURL = 'mongodb+srv://prabhavgiriya925:Prabhav12345@cluster0.0mlnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
